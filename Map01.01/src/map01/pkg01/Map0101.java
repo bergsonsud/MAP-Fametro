1aea83048da7860680757a76ada45219e8c0920d
Bergson Lima <bergsonsud@gmail.com>
2016-02-27 03:47:57 -0300

    Projeto criado e estavel -- projeto criado para disciplina de MAP Fametro'

---
+++ Map01.01/src/map01/pkg01/Map0101.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package map01.pkg01;
+
+import Telas.Inicio;
+
+/**
+ *
+ * @author BKTECH
+ */
+public class Map0101 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Inicio i = new Inicio();
+        i.setVisible(true);
+        
+        
+        
+    }
+    
+}
