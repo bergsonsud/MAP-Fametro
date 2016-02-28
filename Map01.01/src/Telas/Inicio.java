d5904ffd3c6a2ab8835016b499c75c826ca35c1c
Bergson Lima <bergsonsud@gmail.com>
2016-02-27 23:38:08 -0300

    Ajuste nome dos componetes e alerta msg no botao descriptografar'

--- Map01.01/src/Telas/Inicio.java
+++ Map01.01/src/Telas/Inicio.java
@@ -34,38 +34,38 @@ public class Inicio extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jTextField1 = new javax.swing.JTextField();
-        jTextField2 = new javax.swing.JTextField();
-        jButton1 = new javax.swing.JButton();
-        jLabel1 = new javax.swing.JLabel();
-        jLabel2 = new javax.swing.JLabel();
-        jLabel3 = new javax.swing.JLabel();
-        jLabel4 = new javax.swing.JLabel();
-        jTextField3 = new javax.swing.JTextField();
-        jTextField4 = new javax.swing.JTextField();
-        jButton2 = new javax.swing.JButton();
+        TextFieldVogal1 = new javax.swing.JTextField();
+        TextFieldCripto1 = new javax.swing.JTextField();
+        btn_criptografar = new javax.swing.JButton();
+        label_vogal1 = new javax.swing.JLabel();
+        label_cripto1 = new javax.swing.JLabel();
+        label_cripto2 = new javax.swing.JLabel();
+        label_vogal2 = new javax.swing.JLabel();
+        TextFieldCripto2 = new javax.swing.JTextField();
+        TextFieldVogal2 = new javax.swing.JTextField();
+        btn_descriptografar = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        jButton1.setText("Criptografar");
-        jButton1.addActionListener(new java.awt.event.ActionListener() {
+        btn_criptografar.setText("Criptografar");
+        btn_criptografar.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton1ActionPerformed(evt);
+                btn_criptografarActionPerformed(evt);
             }
         });
 
-        jLabel1.setText("Vogal");
+        label_vogal1.setText("Vogal");
 
-        jLabel2.setText("Criptografado");
+        label_cripto1.setText("Criptografado");
 
-        jLabel3.setText("Criptografado");
+        label_cripto2.setText("Criptografado");
 
-        jLabel4.setText("Vogal");
+        label_vogal2.setText("Vogal");
 
-        jButton2.setText("Descriptografar");
-        jButton2.addActionListener(new java.awt.event.ActionListener() {
+        btn_descriptografar.setText("Descriptografar");
+        btn_descriptografar.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton2ActionPerformed(evt);
+                btn_descriptografarActionPerformed(evt);
             }
         });
 
@@ -78,27 +78,27 @@ public class Inicio extends javax.swing.JFrame {
                     .addGroup(layout.createSequentialGroup()
                         .addGap(36, 36, 36)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, 100, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(jLabel1)
-                            .addComponent(jLabel3)
-                            .addComponent(jTextField3, javax.swing.GroupLayout.PREFERRED_SIZE, 100, javax.swing.GroupLayout.PREFERRED_SIZE))
+                            .addComponent(TextFieldVogal1, javax.swing.GroupLayout.PREFERRED_SIZE, 100, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(label_vogal1)
+                            .addComponent(label_cripto2)
+                            .addComponent(TextFieldCripto2, javax.swing.GroupLayout.PREFERRED_SIZE, 100, javax.swing.GroupLayout.PREFERRED_SIZE))
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addGroup(layout.createSequentialGroup()
                                 .addGap(18, 18, 18)
                                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                                    .addComponent(jLabel2)
-                                    .addComponent(jTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, 100, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                                    .addComponent(label_cripto1)
+                                    .addComponent(TextFieldCripto1, javax.swing.GroupLayout.PREFERRED_SIZE, 100, javax.swing.GroupLayout.PREFERRED_SIZE)))
                             .addGroup(layout.createSequentialGroup()
                                 .addGap(18, 18, 18)
                                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                                    .addComponent(jTextField4, javax.swing.GroupLayout.PREFERRED_SIZE, 100, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                    .addComponent(jLabel4)))))
+                                    .addComponent(TextFieldVogal2, javax.swing.GroupLayout.PREFERRED_SIZE, 100, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                    .addComponent(label_vogal2)))))
                     .addGroup(layout.createSequentialGroup()
                         .addGap(103, 103, 103)
-                        .addComponent(jButton1))
+                        .addComponent(btn_criptografar))
                     .addGroup(layout.createSequentialGroup()
                         .addGap(90, 90, 90)
-                        .addComponent(jButton2)))
+                        .addComponent(btn_descriptografar)))
                 .addContainerGap(60, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
@@ -106,34 +106,34 @@ public class Inicio extends javax.swing.JFrame {
             .addGroup(layout.createSequentialGroup()
                 .addGap(52, 52, 52)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabel1)
-                    .addComponent(jLabel2))
+                    .addComponent(label_vogal1)
+                    .addComponent(label_cripto1))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(TextFieldVogal1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(TextFieldCripto1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(jButton1)
+                .addComponent(btn_criptografar)
                 .addGap(38, 38, 38)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabel3)
-                    .addComponent(jLabel4))
+                    .addComponent(label_cripto2)
+                    .addComponent(label_vogal2))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jTextField3, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jTextField4, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(TextFieldCripto2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(TextFieldVogal2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(jButton2)
+                .addComponent(btn_descriptografar)
                 .addContainerGap(51, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+    private void btn_criptografarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn_criptografarActionPerformed
         // TODO add your handling code here:
                
-        String valor = jTextField1.getText();
+        String valor = TextFieldVogal1.getText();
         boolean continuar = false;
 
         
@@ -146,17 +146,17 @@ public class Inicio extends javax.swing.JFrame {
         
         if (continuar){
         int crip = Arrays.binarySearch(vogais, valor);        
-        jTextField2.setText(String.valueOf(crip+1));
+        TextFieldCripto1.setText(String.valueOf(crip+1));
         }else{
             JOptionPane.showMessageDialog(null, "Valor invalido, Digite uma vogal");
-            jTextField1.setText("");
-            jTextField2.setText("");
+            TextFieldVogal1.setText("");
+            TextFieldCripto1.setText("");
         }
-    }//GEN-LAST:event_jButton1ActionPerformed
+    }//GEN-LAST:event_btn_criptografarActionPerformed
 
-    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+    private void btn_descriptografarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn_descriptografarActionPerformed
         int[] numeros = {1,2,3,4,5};        
-        int valor = Integer.parseInt(jTextField3.getText());
+        int valor = Integer.parseInt(TextFieldCripto2.getText());
         boolean continuar = false;
 
         
@@ -170,13 +170,13 @@ public class Inicio extends javax.swing.JFrame {
         
         if (continuar){
         
-        jTextField4.setText(vogais[valor-1]);
+        TextFieldVogal2.setText(vogais[valor-1]);
         }else{
-            JOptionPane.showMessageDialog(null, "Valor invalido, Digite uma vogal");
-            jTextField3.setText("");
-            jTextField4.setText("");
+            JOptionPane.showMessageDialog(null, "Valor invalido, Digite uma número entre 1 e 5");
+            TextFieldCripto2.setText("");
+            TextFieldVogal2.setText("");
         }
-    }//GEN-LAST:event_jButton2ActionPerformed
+    }//GEN-LAST:event_btn_descriptografarActionPerformed
     
     
     /**
@@ -219,15 +219,15 @@ public class Inicio extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton jButton1;
-    private javax.swing.JButton jButton2;
-    private javax.swing.JLabel jLabel1;
-    private javax.swing.JLabel jLabel2;
-    private javax.swing.JLabel jLabel3;
-    private javax.swing.JLabel jLabel4;
-    private javax.swing.JTextField jTextField1;
-    private javax.swing.JTextField jTextField2;
-    private javax.swing.JTextField jTextField3;
-    private javax.swing.JTextField jTextField4;
+    private javax.swing.JTextField TextFieldCripto1;
+    private javax.swing.JTextField TextFieldCripto2;
+    private javax.swing.JTextField TextFieldVogal1;
+    private javax.swing.JTextField TextFieldVogal2;
+    private javax.swing.JButton btn_criptografar;
+    private javax.swing.JButton btn_descriptografar;
+    private javax.swing.JLabel label_cripto1;
+    private javax.swing.JLabel label_cripto2;
+    private javax.swing.JLabel label_vogal1;
+    private javax.swing.JLabel label_vogal2;
     // End of variables declaration//GEN-END:variables
 }
